96f278b7d5d19f5397e1dd2f59b9e3ecae7415a0
JiYounKang <kingeve@gmail.com>
2020-04-16 12:00:54 +0200

    Update

--- template-SIO/socket.js
+++ template-SIO/socket.js
@@ -1,86 +1,41 @@
-//define express server
+//First create an express server
+
 var express = require('express');
 var app = express();
-//create http 
-var http = require('http');
-var fs = require('fs');
-//define socket io
-var socketIO = require('socket.io');
+var http = require('http');//allows Node.js to transfer data over the Hyper Text Transfer Protocol (HTTP).
+var fs = require('fs');//file system library, which allows Node.js to access to the file system
+
+//Now we are using express server for http
 var server = http.Server(app);
-var io = socketIO(server);
-//load dgram
-var dgram = require('dgram');
-//load osc library
-var osc = require('osc-min');
 
-var remoteOscIp;
 
+
+//// Loading the index file . html displayed to the client
 app.get('/', function(req,res){
-  fs.readFile(__dirname + '/index.html', function(error, content) {
-    if(error) {
-      res.writeHead(500);
-      return res.end('Error loading index.html');
-    }
-    res.writeHead(200);
-    res.end(content);
-
-  });
-});
-
-
-//RECEIVE the OSC messages
-//open udpServer
-var udpServer = dgram.createSocket('udp4', function(msg, rinfo) {
-
-  var oscMessage;
-  try {
-    oscMessage = osc.fromBuffer(msg);//get the OSC messages
-    console.log(oscMessage);
-  } catch(err) {
-    return console.log('Could not decode OSC message');
-  }
-  //in case of a wrong address
-  if(oscMessage.address != '/socketio') {
-    return console.log('Invalid OSC address');
-  }
-  //prepare to send this received messages to another
-  remoteOscIp = rinfo.address;
-
-  //assign the value
-  io.emit('osc', {
-    x: parseInt(oscMessage.args[0].value) || 0,//|| 0 --> if there is no message, then send 0
-    y: parseInt(oscMessage.args[1].value) || 0
-  });
-
-});
-//SEND OSC messages
-io.on('connection', function(socket) {
-  socket.on('browser', function(data) {
-    if(! remoteOscIp) {
-      return;
-    }
-    var oscMsg = osc.toBuffer({
-      oscType: 'message',
-      address: '/socketio',
-      args:[{ 
-        type: 'integer',
-        value: parseInt(data.x) || 0
-      },
-      {
-        type: 'integer',
-        value: parseInt(data.y) || 0
-      }]
+    //fs.readFile: https://nodejs.org/docs/latest-v13.x/api/fs.html#fs_fs_readfile_path_options_callback
+    fs.readFile('./index2.html', 'utf-8', function(error, content) {
+      //In case of an error, it creates a html file with the error content.
+      if(error) {
+        res.writeHead(500);//(status code 200)
+        return res.end('Error loading index.html');
+      }
+      //loading index.html file
+        res.writeHead(200, {"Content-Type": "text/html"}); //writes the HTTP header (status code 200)
+        res.end(content);//writes the body and closes the response
     });
-    //socket.send(msg[, offset, length][, port][, address][, callback])
-    udpServer.send(oscMsg, 0, oscMsg.length, 57120, remoteOscIp);
-    console.log('Sent OSC message to %s:9999', remoteOscIp);
-
   });
 
-});
+// Loading socket.io
+var io = require('socket.io').listen(server);
 
-server.listen(8000);
-console.log('Starting HTTP server on TCP port 8000');
-udpServer.bind(9998);
-console.log('Starting UDP server on UDP port 9998');
+//When a client connects, we note it in the console
 
+
+//Server side message when connected
+io.sockets.on('connection', function (socket) {
+    socket.emit('news', { hello: 'world' });//emit an event to the 'news' socket
+    socket.on('my other event', function (data) {//listen to the 'my other event' socket
+      console.log(data);
+    });
+  });
+server.listen(8080);
